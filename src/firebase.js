de93727fe09b3d8ff96a7fa385858533de336d8a
SAKSIT-B <skst.b13@gmail.com>
2025-11-29 20:07:55 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,22 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// 👉 ใส่ค่า config จาก Firebase console ของครู
+// เอาค่าพวกนี้จาก Firebase console ของครู
 const firebaseConfig = {
- apiKey: "AIzaSyAZDEq9KC4WnaYMWSJOrBw3iwbnsewGiiM",
+  apiKey: "AIzaSyAZDEq9KC4WnaYMWSJOrBw3iwbnsewGiiM",
   authDomain: "nutrition-find.firebaseapp.com",
   projectId: "nutrition-find",
   storageBucket: "nutrition-find.firebasestorage.app",
   messagingSenderId: "932843302530",
   appId: "1:932843302530:web:a50fd394bd0546a8a00e9b",
-  measurementId: "G-BEP2XS15CS"',
-}
+  measurementId: "G-BEP2XS15CS"
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-export default app
+export default app;
