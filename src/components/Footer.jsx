b2605beaef72824e9c234605fd18e44537bd7a99
SAKSIT-B <skst.b13@gmail.com>
2025-11-29 21:52:04 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// src/components/Footer.jsx
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <p>
+        จัดทำโดย <strong>ครูศักดิ์สิทธิ์ บำรุง</strong> | แผนกวิชาอาหารและโภชนาการ
+        วิทยาลัยอาชีวศึกษาสุโขทัย • แหล่งข้อมูลจาก สำนักโภชนาการ กรมอนามัย (Thai FCD)
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
