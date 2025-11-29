3653df7cc4e429d3e693e06d5980c3ac05fc0528
SAKSIT-B <skst.b13@gmail.com>
2025-11-29 19:45:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nutrition-app/'
+  base: '/nutrition-app/', // ต้องตรงกับชื่อ repo บน GitHub
 })
