828faba4490fd3561e3642a23e1bce5dc8f750d6
SAKSIT-B <skst.b13@gmail.com>
2026-01-08 17:00:53 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
@@ -6,10 +7,14 @@ import ForgotPassword from './pages/ForgotPassword'
 import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 import Footer from './components/Footer'  
+import SessionAlert from './components/SessionAlert'
 
 const App = () => {
   return (
     <>
+      {/* Session Alert - แสดง Modal แจ้งเตือนเมื่อถูก Logout */}
+      <SessionAlert />
+      
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
@@ -25,7 +30,7 @@ const App = () => {
         />
         <Route path="*" element={<div>ไม่พบหน้านี้</div>} />
       </Routes>
-
+      
       {/* เครดิตด้านล่างทุกหน้า */}
       <Footer />
     </>
